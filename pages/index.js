e3ff03ba7d90a2c2027153d7920d7be5e650a910
Yuriy Dybskiy <yuriy@dybskiy.com>
2018-03-08 21:00:50 -0800

    Add batman show list :)

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
 import Layout from '../components/MyLayout'
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const PostLink = (props) => (
-    <li>
-        <Link as={`/post/${props.id}`} href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-)
-
-export default () => (
-    <Layout>
-        <h1>Hello Next.js</h1>    
+const Index = (props) => (
+       <Layout>
+        <h1>Batman TV shows</h1>
         <ul>
-            <PostLink id="hello-next" title="Hello Next.js" />
-            <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-            <PostLink id="deploy-nextjs" title="Deploy apps with now" />
+            {props.shows.map(( {show} ) => (
+                <li key={show.id}>
+                    <Link as={`/post/${show.id}`} href={`/post?id=${show.title}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
         </ul>
     </Layout>
 )
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json()
+
+    console.log(`Show data fetch count: ${data.length}`)
+
+    return {
+        shows: data
+    }
+}
+
+export default Index
