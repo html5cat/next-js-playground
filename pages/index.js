52c07733a63f1b4db7aba3c3afc8de406296e6df
Yuriy Dybskiy <yuriy@dybskiy.com>
2018-03-11 15:14:23 -0700

    Add css in js styles

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,48 @@ import Layout from '../components/MyLayout'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 
+const ShowLink = ({ show }) => (
+    <li>
+        <Link as={`/post/${show.id}`} href={`/post?id=${show.title}`}>
+            <a>{show.name}</a>
+        </Link>
+    <style jsx>{`
+        li {
+            list-style: none;
+            margin: 5px 0;
+        }
+
+        a {
+            text-decoration: none;
+            color: blue;
+        }
+
+        a:hover {
+            opacity: 0.6;
+        }
+    `}
+    </style>
+    </li>
+)
+
 const Index = (props) => (
        <Layout>
         <h1>Batman TV shows</h1>
         <ul>
             {props.shows.map(( {show} ) => (
-                <li key={show.id}>
-                    <Link as={`/post/${show.id}`} href={`/post?id=${show.title}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
+                <ShowLink show={show} key={show.id} />
             ))}
         </ul>
+        <style jsx>{`
+            h1, a {
+                font-family: "Arial";
+            }
+
+            ul {
+                padding: 0;
+            }
+        `}</style>
+
     </Layout>
 )
 
